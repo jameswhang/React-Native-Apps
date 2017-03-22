512dc62d6208840ad18a92745ea5080e3746776f
jameswhang <sungyoonwhang2017@u.northwestern.edu>
2017-03-22 01:03:27 -0700

    auth doen

---
+++ auth/src/app.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import firebase from 'firebase';
+import { Header, Button, Spinner } from './components/common';
+import LoginForm from './components/LoginForm';
+
+class App extends Component {
+  state = { loggedIn: null };
+
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyC1bYziVfz460Lw-xAoEwERAe3jiLl5XHc',
+      authDomain: 'auth-4237b.firebaseapp.com',
+      databaseURL: 'https://auth-4237b.firebaseio.com',
+      storageBucket: 'auth-4237b.appspot.com',
+      messagingSenderId: '725718770809'
+    });
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
+    });
+  }
+
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true:
+        return (
+          <Button onPress={() => firebase.auth().signOut()}>
+            Log Out
+          </Button>
+        );
+      case false:
+        return <LoginForm />;
+      default:
+        return <Spinner />;
+    }
+  }
+
+  render() {
+    return (
+      <View>
+        <Header headerText="Authentication" />
+        {this.renderContent()}
+      </View>
+    );
+  }
+}
+
+export default App;
